2ec32edaa6780fe728ca17b479bfb114c86a6f8f
AIE\taighe.warner-hunt <taighe.wh@gmail.com>
2017-08-03 17:18:05 +1000

    VR Drop Snap Event System in progress

---
+++ Assets/Script/DropZoneSnap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZoneSnap : MonoBehaviour
+{
+    public GameObject highLightZone;
+    public Material highLightMaterial;
+    GameObject m_highLightZone;
+	// Use this for initialization
+	void Start ()
+    {
+        m_highLightZone = highLightZone;
+        m_highLightZone.GetComponent<MeshRenderer>().material = highLightMaterial;
+        m_highLightZone.transform.SetParent(transform);
+        m_highLightZone.SetActive(false);
+    }
+	
+    void OnTriggerStay(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("Interactable"))
+        {
+            m_highLightZone.SetActive(true);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Interactable"))
+        {
+            m_highLightZone.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+}
